599db84fc1d35ac90dbecc3b50b6a6e78bacb457
Michael Frieze <michaelfrieze@icloud.com>
2023-02-13 17:54:31 -0500

    added my netlify domain to the astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-cip.netlify.app',
+  site: 'https://astro-blog-michaelfrieze.netlify.app',
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
