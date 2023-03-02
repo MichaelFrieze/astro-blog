8e30a1609f4852f555e6809d7793e44f6f5b5031
Michael Frieze <michaelfrieze@icloud.com>
2023-03-02 13:22:19 -0500

    removed github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-michaelfrieze.netlify.app',
+  site: 'https://astro-blog-michaelfrieze.vercel.app',
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
