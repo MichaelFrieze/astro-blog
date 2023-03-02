353ebd49871aaaf0df06af5659bc576bb88ba820
Michael Frieze <michaelfrieze@icloud.com>
2023-03-02 09:53:05 -0500

    SSR is working

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,16 @@ import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-michaelfrieze.netlify.app',
-  integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    sitemap(),
-  ],
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
